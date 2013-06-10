724ad5ace920e58a7e0511d42f9070f73c0c26b7
John Landahl <john@landahl.org>
2013-06-10 13:36:56 -0700

    TestOrderSummary now uses Order objects

--- src/test/scala/org/landahl/emdr/TestOrderSummary.scala
+++ src/test/scala/org/landahl/emdr/TestOrderSummary.scala
@@ -3,19 +3,37 @@ package org.landahl.emdr
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers._
 
-import model.{OrderRow, OrderSummary}
+import org.landahl.emdr.model.{Order, OrderSummary}
 
 class TestOrderSummary extends FunSuite {
+  def newOrder(price: Double, bid: Boolean) = {
+    Order(
+      generatedAt = "",
+      regionID = Some(1), 
+      solarSystemID = Some(1),
+      stationID = 1,
+      typeID = 1,
+      orderID = 1,
+      bid = bid,
+      price = price,
+      volEntered = 10,
+      volRemaining = 10,
+      range = 1,
+      minVolume = 1,
+      issueDate = "",
+      duration = 1)
+  }
+
   test("empty list") {
     OrderSummary.summarize(List()) should not be ('defined)
   }
   
   val sellOrders = List(
-    OrderRow(solarSystemID=Some(1), stationID=1, orderID=0, bid=false, price=10.50, volEntered=10, volRemaining=10, range=1, minVolume=1, issueDate="", duration=1),
-    OrderRow(solarSystemID=Some(1), stationID=1, orderID=0, bid=false, price=10.75, volEntered=10, volRemaining=10, range=1, minVolume=1, issueDate="", duration=1),
-    OrderRow(solarSystemID=Some(1), stationID=1, orderID=0, bid=false, price=11.00, volEntered=10, volRemaining=10, range=1, minVolume=1, issueDate="", duration=1),
-    OrderRow(solarSystemID=Some(1), stationID=1, orderID=0, bid=false, price=11.25, volEntered=10, volRemaining=10, range=1, minVolume=1, issueDate="", duration=1),
-    OrderRow(solarSystemID=Some(1), stationID=1, orderID=0, bid=false, price=75.00, volEntered=10, volRemaining=10, range=1, minVolume=1, issueDate="", duration=1)
+    newOrder(10.50, false),
+    newOrder(10.75, false),
+    newOrder(11.00, false),
+    newOrder(11.25, false),
+    newOrder(75.00, false)
   )
 
   test("sellOrders") {
@@ -32,11 +50,11 @@ class TestOrderSummary extends FunSuite {
   }
 
   val buyOrders = List(
-    OrderRow(solarSystemID=Some(1), stationID=1, orderID=0, bid=true, price=9.00, volEntered=10, volRemaining=10, range=1, minVolume=1, issueDate="", duration=1),
-    OrderRow(solarSystemID=Some(1), stationID=1, orderID=0, bid=true, price=9.01, volEntered=10, volRemaining=10, range=1, minVolume=1, issueDate="", duration=1),
-    OrderRow(solarSystemID=Some(1), stationID=1, orderID=0, bid=true, price=9.50, volEntered=10, volRemaining=10, range=1, minVolume=1, issueDate="", duration=1),
-    OrderRow(solarSystemID=Some(1), stationID=1, orderID=0, bid=true, price=9.51, volEntered=10, volRemaining=10, range=1, minVolume=1, issueDate="", duration=1),
-    OrderRow(solarSystemID=Some(1), stationID=1, orderID=0, bid=true, price=9.60, volEntered=10, volRemaining=10, range=1, minVolume=1, issueDate="", duration=1)
+    newOrder(9.00, true),
+    newOrder(9.01, true),
+    newOrder(9.50, true),
+    newOrder(9.51, true),
+    newOrder(9.60, true)
   )
 
   test("buyOrders") {
