c2299707c1f58ac493bdc2eff641b6115ecda661
John Landahl <john@landahl.org>
2013-06-19 16:54:06 -0700

    Moved code from uudifRows.scala back to UUDIF.scala

--- src/main/scala/org/landahl/emdr/model/UUDIF.scala
+++ src/main/scala/org/landahl/emdr/model/UUDIF.scala
@@ -19,3 +19,28 @@ case class UploadKey(
 
 case class Generator(name: String, version: String)
 case class Rowset(generatedAt: String, regionID: Option[Int], typeID: Int, rows: List[Row])
+
+trait Row
+
+case class OrderRow(
+  solarSystemID: Option[Int],    // possibly null
+  stationID: Int,
+  orderID: Long,
+  bid: Boolean,
+  price: Double,
+  volEntered: Long,
+  volRemaining: Long,
+  range: Int,
+  minVolume: Int,
+  issueDate: String,
+  duration: Int
+) extends Row
+
+case class HistoryRow(
+  date: String,
+  orders: Int,
+  quantity: Int,
+  low: Double,
+  high: Double,
+  average: Double
+) extends Row
