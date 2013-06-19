62e9d58688b44b6dd6f53fecdd6a0e34eb5b86ef
John Landahl <john@landahl.org>
2013-06-19 16:36:27 -0700

    Added Mongo connection

--- src/main/scala/org/landahl/emdr/Processor.scala
+++ src/main/scala/org/landahl/emdr/Processor.scala
@@ -6,11 +6,13 @@ import org.apache.camel.scala.dsl.builder.RouteBuilderSupport
 object Processor extends App with RouteBuilderSupport {
   import org.apache.camel.main.Main
   import org.apache.activemq.camel.component.ActiveMQComponent
+  import com.mongodb.Mongo
 
   val config = ConfigFactory.load
   val main = new Main
   //main.enableHangupSupport
   main.addRouteBuilder(new Routes(config))
   main.bind("activemq", ActiveMQComponent.activeMQComponent(config.getString("activemq.url")))
+  main.bind("db", new Mongo(config.getString("mongodb.host"), config.getInt("mongodb.port")))
   main.run
 }
