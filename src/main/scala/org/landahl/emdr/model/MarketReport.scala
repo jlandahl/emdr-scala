220f0ecb004392be2851ae8d747876ad60f87074
John Landahl <john@landahl.org>
2013-07-01 15:38:08 -0700

    MarketReport now builds from a list of Orders rather than a UUDIF

--- src/main/scala/org/landahl/emdr/model/MarketReport.scala
+++ src/main/scala/org/landahl/emdr/model/MarketReport.scala
@@ -2,24 +2,22 @@ package org.landahl.emdr.model
 
 case class MarketReport(
   generatedAt: java.util.Date,
-  regionID: Int, 
-  solarSystemID: Int, 
+  regionID: Int,
+  solarSystemID: Int,
   stationID: Int,
   typeID: Int,
-  buy: Option[OrderSummary], 
-  sell: Option[OrderSummary]
-)
+  buy: Option[OrderSummary],
+  sell: Option[OrderSummary])
 
 object MarketReport {
-  def fromUUDIF(uudif: UUDIF, orderFilter: (Order) => Boolean = (order) => true): Iterable[MarketReport] = {
+  def fromOrders(orders: Iterable[Order], orderFilter: (Order) => Boolean = (order) => true): Iterable[MarketReport] = {
     for {
-      rowset <- uudif.rowsets
-      orders = Order.fromRowset(rowset).filter(orderFilter)
-      group <- orders.groupBy(o => (o.generatedAt, o.regionID, o.solarSystemID, o.stationID, o.typeID))
+      group <- orders.filter(orderFilter).groupBy(o => (o.generatedAt, o.regionID, o.solarSystemID, o.stationID, o.typeID))
       ((generatedAt, regionID, solarSystemID, stationID, typeID), groupedOrders) = group
       (buyOrders, sellOrders) = groupedOrders.partition(_.bid)
-    } yield MarketReport(generatedAt, regionID, solarSystemID, stationID, typeID,
-                         buy = OrderSummary.summarize(buyOrders),
-                         sell = OrderSummary.summarize(sellOrders))
+    } yield MarketReport(
+      generatedAt, regionID, solarSystemID, stationID, typeID,
+      buy = OrderSummary.summarize(buyOrders),
+      sell = OrderSummary.summarize(sellOrders))
   }
 }
