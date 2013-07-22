9378873dfab06769d3b42c69d1e0e1004f63d50b
John Landahl <john@landahl.org>
2013-07-22 15:20:15 -0700

    Added actors.KafkaConsumer

---
+++ src/main/scala/org/landahl/emdr/actors/KafkaConsumer.scala
@@ -0,0 +1,50 @@
+package org.landahl.emdr.actors
+
+import akka.actor.Actor
+import kafka.consumer.{ Consumer, ConsumerConfig }
+
+import org.landahl.emdr.Settings
+import org.landahl.emdr.util.Zip
+
+class KafkaConsumer extends Actor {
+  case object Poll
+
+  val settings = Settings(context.system)
+
+  def consumerConfig = {
+    val props = new java.util.Properties
+    props.put("zookeeper.connect", settings.zookeeperURI)
+    props.put("group.id", settings.kafkaGroupId)
+    props.put("zookeeper.session.timeout.ms", "400")
+    props.put("zookeeper.sync.time.ms", "200")
+    props.put("auto.commit.interval.ms", "1000")
+    props.put("auto.offset.reset", "smallest")
+    new ConsumerConfig(props)
+  }
+  val consumer = Consumer.create(consumerConfig)
+  val topicCountMap = Map(settings.kafkaTopic -> 1)
+  val consumerMap = consumer.createMessageStreams(topicCountMap)
+  val stream = consumerMap(settings.kafkaTopic)(0)
+  val iterator = stream.iterator
+
+  override def preStart = {
+    super.preStart
+    self ! Poll
+  }
+
+  def receive = {
+    case Poll => poll
+
+    case data: Array[Byte] => {
+      val json = Zip.inflate(data)
+      println(json)
+    }
+
+    case _ =>
+  }
+
+  private def poll = {
+    self ! iterator.next.message
+    self ! Poll
+  }
+}
