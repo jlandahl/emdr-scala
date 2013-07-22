e10de9593c016e4128656cdf34473c25ba1ea121
John Landahl <john@landahl.org>
2013-07-22 15:19:10 -0700

    Added Settings as an Akka extension to hold app-wide configuration

---
+++ src/main/scala/org/landahl/emdr/Settings.scala
@@ -0,0 +1,24 @@
+package org.landahl.emdr
+
+import akka.actor.{ ActorSystem, Extension, ExtensionId, ExtensionIdProvider, ExtendedActorSystem }
+import scala.concurrent.duration.Duration
+import com.typesafe.config.Config
+import java.util.concurrent.TimeUnit
+
+class Settings(config: Config) extends Extension {
+  val zookeeperURI = config.getString("emdr.zookeeperURI")
+  val kafkaGroupId = config.getString("emdr.kafka.groupID")
+  val kafkaTopic = config.getString("emdr.kafka.topic")
+  val kafkaBrokers = config.getString("emdr.kafka.brokers")
+}
+
+object Settings extends ExtensionId[Settings] with ExtensionIdProvider {
+  override def lookup = Settings
+
+  override def createExtension(system: ExtendedActorSystem) = new Settings(system.settings.config)
+
+  /**
+   * Java API: retrieve the Settings extension for the given system.
+   */
+  override def get(system: ActorSystem): Settings = super.get(system)
+}
