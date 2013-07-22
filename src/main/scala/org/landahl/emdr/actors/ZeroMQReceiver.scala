3ca58bc692d5483fba0a570dd3634f5fc348b7ee
John Landahl <john@landahl.org>
2013-07-22 15:29:05 -0700

    Minor simplification using ZeroMQExtension.newSubSocket convenience method

--- src/main/scala/org/landahl/emdr/actors/ZeroMQReceiver.scala
+++ src/main/scala/org/landahl/emdr/actors/ZeroMQReceiver.scala
@@ -1,10 +1,10 @@
 package org.landahl.emdr.actors
 
 import akka.actor.{ Actor, ActorRef, Props }
-import akka.zeromq.{ ZMQMessage, ZeroMQExtension, SocketType, Listener, Connect, SubscribeAll }
+import akka.zeromq.{ ZMQMessage, ZeroMQExtension, Listener, Connect, SubscribeAll }
 
 class ZeroMQReceiver(uri: String, queueProducer: ActorRef) extends Actor {
-  ZeroMQExtension(context.system).newSocket(SocketType.Sub, Listener(self), Connect(uri), SubscribeAll)
+  ZeroMQExtension(context.system).newSubSocket(Connect(uri), Listener(self), SubscribeAll)
 
   def receive = {
     case message: ZMQMessage => {
@@ -12,7 +12,7 @@ class ZeroMQReceiver(uri: String, queueProducer: ActorRef) extends Actor {
       // pass the zipped data directly to the queue
       queueProducer ! data
     }
-    case _ => {}
+    case _ =>
   }
 }
 
