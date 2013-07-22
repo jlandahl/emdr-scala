4133c192c30f42b2365a975dec28b772d054b03d
John Landahl <john@landahl.org>
2013-07-22 15:19:54 -0700

    Added actors.KafkaProducer

---
+++ src/main/scala/org/landahl/emdr/actors/KafkaProducer.scala
@@ -0,0 +1,26 @@
+package org.landahl.emdr.actors
+
+import akka.actor.{ Actor, Props }
+import kafka.javaapi.producer.Producer
+import kafka.producer.{ ProducerConfig, KeyedMessage }
+
+import org.landahl.emdr.Settings
+
+class KafkaProducer extends Actor {
+  val settings = Settings(context.system)
+
+  val producerConfig = {
+    val props = new java.util.Properties
+    props.put("serializer.class", "kafka.serializer.DefaultEncoder")
+    props.put("metadata.broker.list", settings.kafkaBrokers)
+    new ProducerConfig(props)
+  }
+  val producer = new Producer[Int, Array[Byte]](producerConfig)
+
+  def receive = {
+    case data: Array[Byte] => {
+      producer.send(new KeyedMessage[Int, Array[Byte]](settings.kafkaTopic, data))
+    }
+  }
+
+}
