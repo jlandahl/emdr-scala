506642da47ca9f479e7af224637ad548e50dc3f8
John Landahl <john@landahl.org>
2013-07-30 14:30:30 -0700

    Added actor-based Processor2 main class

---
+++ src/main/scala/org/landahl/emdr/Processor2.scala
@@ -0,0 +1,12 @@
+package org.landahl.emdr
+
+import akka.actor.{ ActorSystem, Props }
+import org.landahl.emdr.actors.{ KafkaConsumer, UUDIFProcessor, HistoryStore }
+import akka.testkit.TestProbe
+
+object Processor2 extends App {
+  implicit val system = ActorSystem("EMDR")
+  val historyStore = system.actorOf(HistoryStore.props)
+  val processor = system.actorOf(UUDIFProcessor.props(historyStore))
+  val queueProducer = system.actorOf(KafkaConsumer.props(processor))
+}
