dd7a3da8eeb6facff2688e52ed87e63795b55c32
John Landahl <john@landahl.org>
2013-08-08 15:43:42 -0700

    Added EsperOrderProcessor

--- src/main/scala/org/landahl/emdr/Processor2.scala
+++ src/main/scala/org/landahl/emdr/Processor2.scala
@@ -1,12 +1,13 @@
 package org.landahl.emdr
 
 import akka.actor.{ ActorSystem, Props }
-import org.landahl.emdr.actors.{ KafkaConsumer, UUDIFProcessor, HistoryStore }
+import org.landahl.emdr.actors.{EsperOrderProcessor, KafkaConsumer, UUDIFProcessor, HistoryStore}
 import akka.testkit.TestProbe
 
 object Processor2 extends App {
   implicit val system = ActorSystem("EMDR")
   val historyStore = system.actorOf(HistoryStore.props)
-  val processor = system.actorOf(UUDIFProcessor.props(historyStore))
-  val queueConsumer = system.actorOf(KafkaConsumer.props(processor))
+  val orderProcessor = system.actorOf(EsperOrderProcessor.props)
+  val uudifProcessor = system.actorOf(UUDIFProcessor.props(historyStore, orderProcessor))
+  val queueConsumer = system.actorOf(KafkaConsumer.props(uudifProcessor))
 }
