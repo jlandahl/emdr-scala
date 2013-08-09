1421046d80b49e422a593b4284236baa8a6ea7fe
John Landahl <john@landahl.org>
2013-08-08 19:44:54 -0700

    Gave names to actors

--- src/main/scala/org/landahl/emdr/Processor2.scala
+++ src/main/scala/org/landahl/emdr/Processor2.scala
@@ -6,8 +6,8 @@ import akka.testkit.TestProbe
 
 object Processor2 extends App {
   implicit val system = ActorSystem("EMDR")
-  val historyStore = system.actorOf(HistoryStore.props)
-  val orderProcessor = system.actorOf(EsperOrderProcessor.props)
-  val uudifProcessor = system.actorOf(UUDIFProcessor.props(historyStore, orderProcessor))
-  val queueConsumer = system.actorOf(KafkaConsumer.props(uudifProcessor))
+  val historyStore = system.actorOf(HistoryStore.props, "historyStore")
+  val orderProcessor = system.actorOf(EsperOrderProcessor.props, "orderProcessor")
+  val uudifProcessor = system.actorOf(UUDIFProcessor.props(historyStore, orderProcessor), "uudifProcessor")
+  val queueConsumer = system.actorOf(KafkaConsumer.props(uudifProcessor), "queueConsumer")
 }
