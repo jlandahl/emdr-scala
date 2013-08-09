8b296254c2fdf8b019be64ed8807c23ea7fe6b44
John Landahl <john@landahl.org>
2013-08-09 10:57:11 -0700

    Added MongoRouting class

---
+++ src/main/scala/org/landahl/emdr/camel/MongoRouting.scala
@@ -0,0 +1,15 @@
+package org.landahl.emdr
+
+import org.apache.camel.scala.dsl.builder.RouteBuilder
+
+import org.landahl.emdr.model.MarketReport
+import org.landahl.emdr.converters.MarketReportToMongoUpdate
+
+class MongoRouting extends RouteBuilder {
+  from("direct:market-reports-to-mongo") ==> {
+    split(_.in[List[MarketReport]]) {
+      process { exchange => MarketReportToMongoUpdate.camelMongoUpdate(exchange) }
+      to("mongodb:db?database=eve&collection=market_reports&operation=update")
+    }
+  }
+}
