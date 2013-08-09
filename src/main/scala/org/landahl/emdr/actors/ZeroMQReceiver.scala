3e1763ece316ea82fd6101f866f8c71e15df78ff
John Landahl <john@landahl.org>
2013-08-09 10:55:12 -0700

    Modified to use Settings extension for configuration; refactored queueProducer variable

--- src/main/scala/org/landahl/emdr/actors/ZeroMQReceiver.scala
+++ src/main/scala/org/landahl/emdr/actors/ZeroMQReceiver.scala
@@ -3,19 +3,22 @@ package org.landahl.emdr.actors
 import akka.actor.{ Actor, ActorRef, Props, ActorLogging }
 import akka.zeromq.{ ZMQMessage, ZeroMQExtension, Listener, Connect, SubscribeAll }
 
-class ZeroMQReceiver(uri: String, queueProducer: ActorRef) extends Actor with ActorLogging {
-  ZeroMQExtension(context.system).newSubSocket(Connect(uri), Listener(self), SubscribeAll)
+import org.landahl.emdr.Settings
+
+class ZeroMQReceiver(processor: ActorRef) extends Actor with ActorLogging {
+  val settings = Settings(context.system)
+
+  ZeroMQExtension(context.system).newSubSocket(Connect(settings.emdrURI), Listener(self), SubscribeAll)
 
   def receive = {
     case message: ZMQMessage => {
-      val data = message.frames(0).toArray
-      // pass the zipped data directly to the queue
-      queueProducer ! data
+      val data: Array[Byte] = message.frames(0).toArray
+      processor ! data
     }
     case x => log.warning("Received unknown message: {}", x)
   }
 }
 
 object ZeroMQReceiver {
-  def props(uri: String, queueProducer: ActorRef) = Props(classOf[ZeroMQReceiver], uri, queueProducer)
+  def props(processor: ActorRef) = Props(new ZeroMQReceiver(processor))
 }
