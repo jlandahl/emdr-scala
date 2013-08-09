d1716740cea3814369db8134ea159aecf37b5910
John Landahl <john@landahl.org>
2013-08-09 10:59:25 -0700

    Added MarketReportToMongoUpdate, RowsetToOrders, and UUDIFToRowsets converters

---
+++ src/main/scala/org/landahl/emdr/converters/RowsetToOrders.scala
@@ -0,0 +1,31 @@
+package org.landahl.emdr.converters
+
+import java.util.{ List => JList }
+import scala.collection.JavaConverters._
+import org.apache.camel.Converter
+import org.landahl.emdr.model.{ Rowset, Order, OrderRow }
+
+object RowsetToOrders {
+  @Converter
+  def extractOrders(rowset: Rowset): JList[Order] = {
+    rowset.rows.collect {
+      case row: OrderRow =>
+        Order(
+          generatedAt = rowset.generatedAt,
+          generatedAt_ms = rowset.generatedAt.getTime,
+          regionID = rowset.regionID.getOrElse(0),
+          solarSystemID = row.solarSystemID.getOrElse(0),
+          stationID = row.stationID,
+          typeID = rowset.typeID,
+          orderID = row.orderID,
+          bid = row.bid,
+          price = row.price,
+          volEntered = row.volEntered,
+          volRemaining = row.volRemaining,
+          range = row.range,
+          minVolume = row.minVolume,
+          issueDate = row.issueDate,
+          duration = row.duration)
+    }.asJava
+  }
+}
