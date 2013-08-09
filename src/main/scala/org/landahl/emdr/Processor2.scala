0833899411bb6c0840539738a1cb28312d5edd38
John Landahl <john@landahl.org>
2013-08-09 11:17:15 -0700

    Modified to allow for direct (non-queued) processing of EMDR data based on new processing-method setting

--- src/main/scala/org/landahl/emdr/Processor2.scala
+++ src/main/scala/org/landahl/emdr/Processor2.scala
@@ -1,13 +1,17 @@
 package org.landahl.emdr
 
-import akka.actor.{ ActorSystem, Props }
-import org.landahl.emdr.actors.{EsperOrderProcessor, KafkaConsumer, UUDIFProcessor, HistoryStore}
-import akka.testkit.TestProbe
+import akka.actor.ActorSystem
+import org.landahl.emdr.actors.{EsperOrderProcessor, KafkaConsumer, UUDIFProcessor, HistoryStore, ZeroMQReceiver}
 
 object Processor2 extends App {
   implicit val system = ActorSystem("EMDR")
   val historyStore = system.actorOf(HistoryStore.props, "historyStore")
   val orderProcessor = system.actorOf(EsperOrderProcessor.props, "orderProcessor")
   val uudifProcessor = system.actorOf(UUDIFProcessor.props(historyStore, orderProcessor), "uudifProcessor")
-  val queueConsumer = system.actorOf(KafkaConsumer.props(uudifProcessor), "queueConsumer")
+
+  val settings = Settings(system)
+  settings.processingMethod match {
+    case "direct" => system.actorOf(ZeroMQReceiver.props(uudifProcessor), "receiver")
+    case "queued" => system.actorOf(KafkaConsumer.props(uudifProcessor), "queueConsumer")
+  }
 }
