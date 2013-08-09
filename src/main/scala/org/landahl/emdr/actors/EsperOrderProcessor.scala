2ad8d7085d14396162f5cc88d0c0d4ba229470e9
John Landahl <john@landahl.org>
2013-08-09 12:59:36 -0700

    dev snapshot

--- src/main/scala/org/landahl/emdr/actors/EsperOrderProcessor.scala
+++ src/main/scala/org/landahl/emdr/actors/EsperOrderProcessor.scala
@@ -21,25 +21,43 @@ class EsperOrderProcessor extends Actor with ActorLogging {
   EsperStatements.createStatements(settings.rowsetFilter, settings.orderFilter)
 
   admin createEPL("select * from Rowsets") addListener(listener)
-  admin createEPL("select * from SellEvents") addListener new UpdateListener {
+  admin createEPL("select * from SellEvents order by volume desc") addListener new UpdateListener {
     def update(newEvents: Array[EventBean], oldEvents: Array[EventBean]) {
-      newEvents.foreach(eb => println(eb.getUnderlying))
+      val events = newEvents.collect {
+        case map: java.util.Map[String,Any] if map.get("sell_price") != null => map
+      }
+      if (events.length > 0) {
+        println
+        events.foreach(println(_))
+      }
+    }
+  }
+
+  admin createEPL("select count(*) as cnt from Orders.win:time(10 sec) output last every 10 seconds") addListener new UpdateListener{
+    def update(newEvents: Array[EventBean], oldEvents: Array[EventBean]) {
+      newEvents.foreach{ eb =>
+        eb.getUnderlying match {
+          case mapEvent: java.util.Map[String, Any] =>
+            println(s"Processed ${mapEvent.get("cnt")} events")
+        }
+      }
     }
   }
 
   val topitems = admin createEPL """
     select typeID, count(*) as cnt
-      from Rowsets.win:time(30 sec)
+      from Rowsets.win:time(60 sec)
   group by typeID
-    output last every 30 seconds
+    output last every 60 seconds
   order by cnt desc
-     limit 10
+     limit 20
   """
 
   topitems addListener new UpdateListener {
     def update(newEvents: Array[EventBean], oldEvents: Array[EventBean]) {
       if (newEvents.length > 0) {
-        println("Top 10 items")
+        println
+        println("Top Items")
         newEvents.foreach(eb => println(eb.getUnderlying))
       }
     }
