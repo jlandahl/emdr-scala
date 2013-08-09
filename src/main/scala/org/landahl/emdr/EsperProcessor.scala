a1ea184310685ef96e83b411b63dd345fc62ebb5
John Landahl <john@landahl.org>
2013-08-09 10:35:36 -0700

    Updated to use new calling syntax of EsperStatements.createStatements

--- src/main/scala/org/landahl/emdr/EsperProcessor.scala
+++ src/main/scala/org/landahl/emdr/EsperProcessor.scala
@@ -1,14 +1,17 @@
 package org.landahl.emdr
 
+import scala.util.control.Exception.catching
 import scala.collection.JavaConversions._
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{ConfigFactory, ConfigException}
 import org.apache.camel.scala.dsl.builder.RouteBuilderSupport
 import org.apache.camel.main.Main
 import org.apache.activemq.camel.component.ActiveMQComponent
 
 object EsperProcessor extends App with RouteBuilderSupport {
   val config = ConfigFactory.load
-  EsperStatements.createStatements(config)
+  val rowsetFilter = catching(classOf[ConfigException.Missing]) opt config.getString("emdr.rowset-filter")
+  val orderFilter =  catching(classOf[ConfigException.Missing]) opt config.getString("emdr.order-filter")
+  EsperStatements.createStatements(rowsetFilter, orderFilter)
   val main = new Main
   //main.enableHangupSupport
   main.addRouteBuilder(new EsperRouting(config))
