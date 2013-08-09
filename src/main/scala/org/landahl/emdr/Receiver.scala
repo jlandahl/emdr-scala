2784a9899af9ad98f61d08c9aca34b90fc46f9e7
John Landahl <john@landahl.org>
2013-08-09 10:41:11 -0700

    Simplified by moving config to Settings class

--- src/main/scala/org/landahl/emdr/Receiver.scala
+++ src/main/scala/org/landahl/emdr/Receiver.scala
@@ -1,15 +1,10 @@
 package org.landahl.emdr
 
-import com.typesafe.config.ConfigFactory
-import akka.actor.{ ActorSystem, Props }
-import org.landahl.emdr.actors.{ ZeroMQReceiver, KafkaProducer }
+import akka.actor.ActorSystem
+import org.landahl.emdr.actors.{ZeroMQReceiver, KafkaProducer}
 
 object Receiver extends App {
-  val config = ConfigFactory.load
-  val emdrURI = config.getString("emdr.uri")
-  val queueURI = config.getString("emdr.input")
-
   val system = ActorSystem("EMDR")
-  val queueProducer = system.actorOf(KafkaProducer.props)
-  val receiver = system.actorOf(ZeroMQReceiver.props(emdrURI, queueProducer), "receiver")
+  val queueProducer = system.actorOf(KafkaProducer.props, "processor")
+  val receiver = system.actorOf(ZeroMQReceiver.props(queueProducer), "receiver")
 }
