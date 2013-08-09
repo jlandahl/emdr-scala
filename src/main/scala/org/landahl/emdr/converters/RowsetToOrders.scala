6406a9ca3ef75309c0ff9e927c3e31a3a9d9e81e
John Landahl <john@landahl.org>
2013-08-09 14:32:41 -0700

    Removed conversion to Java list; refactored to use pattern matching in the for comprehension

--- src/main/scala/org/landahl/emdr/converters/RowsetToOrders.scala
+++ src/main/scala/org/landahl/emdr/converters/RowsetToOrders.scala
@@ -1,29 +1,25 @@
 package org.landahl.emdr.converters
 
-import java.util.{ List => JList }
-import scala.collection.JavaConverters._
 import org.landahl.emdr.model.{ Rowset, Order, OrderRow }
 
 object RowsetToOrders {
-  def extractOrders(rowset: Rowset): JList[Order] = {
-    rowset.rows.collect {
-      case row: OrderRow =>
-        Order(
-          generatedAt = rowset.generatedAt,
-          generatedAt_ms = rowset.generatedAt.getTime,
-          regionID = rowset.regionID.getOrElse(0),
-          solarSystemID = row.solarSystemID.getOrElse(0),
-          stationID = row.stationID,
-          typeID = rowset.typeID,
-          orderID = row.orderID,
-          bid = row.bid,
-          price = row.price,
-          volEntered = row.volEntered,
-          volRemaining = row.volRemaining,
-          range = row.range,
-          minVolume = row.minVolume,
-          issueDate = row.issueDate,
-          duration = row.duration)
-    }.asJava
+  def extractOrders(rowset: Rowset): Seq[Order] = {
+    for(OrderRow(solarSystemID, stationID, orderID, bid, price, volEntered, volRemaining, range, minVolume, issueDate, duration) <- rowset.rows)
+    yield Order(
+      generatedAt = rowset.generatedAt,
+      generatedAt_ms = rowset.generatedAt.getTime,
+      regionID = rowset.regionID.getOrElse(0),
+      solarSystemID = solarSystemID.getOrElse(0),
+      stationID = stationID,
+      typeID = rowset.typeID,
+      orderID = orderID,
+      bid = bid,
+      price = price,
+      volEntered = volEntered,
+      volRemaining = volRemaining,
+      range = range,
+      minVolume = minVolume,
+      issueDate = issueDate,
+      duration = duration)
   }
 }
