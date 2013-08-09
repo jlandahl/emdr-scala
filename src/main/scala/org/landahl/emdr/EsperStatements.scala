635e4f6e68bc15d40cd51a7ed9192cb174d3cb21
John Landahl <john@landahl.org>
2013-08-09 10:38:13 -0700

    Multiple refinements and additions to Esper statements

--- src/main/scala/org/landahl/emdr/EsperStatements.scala
+++ src/main/scala/org/landahl/emdr/EsperStatements.scala
@@ -5,13 +5,34 @@ import com.espertech.esper.client.EPServiceProviderManager
 import com.typesafe.config.Config
 
 object EsperStatements {
-  def createStatements(config: Config) = {
+  def createStatements(rowsetFilter: Option[String], orderFilter: Option[String]) = {
     val epService = EPServiceProviderManager.getDefaultProvider
     val admin = epService.getEPAdministrator
 
-    val stationList = config.getList("order-filter.stations").map(_.render.toInt).toList
-    admin createEPL "insert into FilteredOrders select * from `org.landahl.emdr.model.Order` where stationID in [" + stationList.mkString(",") + "]"
-    admin createEPL "insert into BuyOrders select * from FilteredOrders where bid = true"
-    admin createEPL "insert into SellOrders select * from FilteredOrders where bid = false"
+    admin createEPL "insert into Rowsets select * from org.landahl.emdr.model.Rowset" +
+      (if (rowsetFilter.isDefined) s" where ${rowsetFilter.get}" else "")
+
+    admin createEPL "insert into Orders select * from `org.landahl.emdr.model.Order`" +
+      (if (orderFilter.isDefined) s" where ${orderFilter.get}" else "") +
+      "group by orderID output last every 30 seconds" // remove duplicates
+
+    admin createEPL "insert into BuyOrders select * from Orders where bid = true"
+    admin createEPL """
+      insert into BuyEvents
+      select regionID, solarSystemID, stationID, typeID, max(price) as buy_price, sum(volRemaining) as volume
+        from BuyOrders.win:time(60 sec)
+    """
+
+    admin createEPL "insert into SellOrders select * from Orders where bid = false"
+    admin createEPL """
+      insert into SellEvents
+      select regionID, solarSystemID, stationID, typeID, min(price) as sell_price, sum(volRemaining) as volume
+        from SellOrders.win:time(30 sec)
+    group by regionID, solarSystemID, stationID, typeID
+      output last every 30 seconds
+    //order by volume desc
+    //   limit 10
+    """
+
   }
 }
